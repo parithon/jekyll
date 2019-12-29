b4cec916b1995d104a2e555e1625c278572fc7fb
Anthony Conrad <8602418+parithon@users.noreply.github.com>
2019-12-29 15:48:35 -0600

    Container profiles fixed and support Powershell

--- .devcontainer/profile.ps1
+++ .devcontainer/profile.ps1
@@ -1,3 +1,7 @@
 Import-Module devtoolbox
-Import-Module oh-my-posh
+Import-Module posh-git -DisableNameChecking
+Import-Module oh-my-posh -DisableNameChecking
 Set-Theme Powerlevel10k-Lean
+
+# Load the BASH profile (enable socat for GPG socket forwarding)
+sh ~/.bashrc
